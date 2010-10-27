5bc664ee00919d8c7b74c875807010e2f9eb12ba
pianomanDylan <pianomanDylan@fd496d0a-aefe-dfaf-78f1-ce77a1763a5f>
2010-10-27 15:37:42 +0000

    Removed FileHistory- use BrowserURLHistory instead (under views/browser) Added more settings to the MatchReplaceRule, to match the editor. Added some javadoc comments in various places.

--- file-utils/src/edu/zao/fire/editors/RenamerRuleEditor.java
+++ file-utils/src/edu/zao/fire/editors/RenamerRuleEditor.java
@@ -4,6 +4,15 @@ import org.eclipse.ui.part.EditorPart;
 
 import edu.zao.fire.RenamerRule;
 
+/**
+ * Common ground for the various renamer rule editors.
+ * 
+ * @author dylan
+ * 
+ */
 public abstract class RenamerRuleEditor extends EditorPart {
+	/**
+	 * @return The RenamerRule that this editor makes changes to.
+	 */
 	public abstract RenamerRule getRule();
 }
