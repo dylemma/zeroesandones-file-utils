a7bc1d5a4ebee969ef5198c487f6695bd28a3db1
jsavickas103@gmail.com <jsavickas103@gmail.com@fd496d0a-aefe-dfaf-78f1-ce77a1763a5f>
2010-11-03 17:48:18 +0000

    continued work on ListRule, only need to finish alphabetical now Added RomanConversion, a static class that converts integers to roman numerals.

--- trunk/file-utils/src/edu/zao/fire/ListRule.java
+++ trunk/file-utils/src/edu/zao/fire/ListRule.java
@@ -4,69 +4,108 @@ import java.io.File;
 import java.io.IOException;
 
 public class ListRule implements RenamerRule {
-// TODO: Finish setup(), tearDown() and getNewName().
-// TODO: Change startFrom to an integer, parse accordingly in getNewName
+	// TODO: getAlphabetical
+
 	/**
 	 * A generated serial ID
 	 */
 	private static final long serialVersionUID = 5732001028450514775L;
 	private boolean isAscending = false;
-	private boolean isDescending = false;
 	private boolean isAddToEnd = false;
-	private boolean isAddToStart = false;
 	private String seperatorToken = "";
-	private String startFrom = "";
+	private int startFrom = 0;
 	private int counter = 0;
 
-	
-	public static enum ListStyle{
+	public static enum ListStyle {
 		NONE, NUMERIC, ALPHABETICAL, ROMAN_NUMERALS
 	}
+
 	private ListStyle listStyleState = ListStyle.NONE;
 	private int digitsDisplayed = 0;
-	
-	
+
 	@Override
+	// TODO: Need a decimal to Roman numeral converter
+	// (http://www.roseindia.net/java/java-tips/45examples/misc/roman/roman.shtml)
 	public String getNewName(File file) throws IOException {
-		// TODO: Need a decimal to Roman numeral converter (http://www.roseindia.net/java/java-tips/45examples/misc/roman/roman.shtml)
-		return null;
+		String numeral = "";
+		String oldName = file.getName();
+		String newName = "";
+		switch (listStyleState) {
+		case NUMERIC:
+			numeral = getNumeric();
+		case ALPHABETICAL:
+			numeral = getAlphabetical();
+		case ROMAN_NUMERALS:
+			numeral = getRoman();
+		}
+
+		if (isAddToEnd)
+			newName = numeral + numeral;
+		else
+			newName = oldName + numeral;
+
+		if (isAscending)
+			counter++;
+		else
+			counter--;
+
+		return newName;
 	}
-	
+
+	private String getNumeric() {
+		return String.format("%0" + Integer.toString(digitsDisplayed) + "d", counter);
+	}
+
+	// http://stackoverflow.com/questions/342052/how-to-increment-a-java-string-through-all-the-possibilities
+	private String getAlphabetical() {
+		return "";
+	}
+
+	private String getRoman() {
+		return RomanConversion.binaryToRoman(counter);
+	}
+
 	/**
 	 * Set the value to start the list from
 	 * 
 	 * @param startFrom
 	 */
-	public void setStartFrom(String startFrom){
+	public void setStartFrom(int startFrom) {
 		this.startFrom = startFrom;
 	}
-	public String getStartFrom(){
+
+	public int getStartFrom() {
 		return startFrom;
 	}
+
 	/**
 	 * Set the seperatorToken to a desired value
 	 * 
 	 * @param seperatorToken
-	 * 				The new value of seperatorToken
+	 *            The new value of seperatorToken
 	 */
-	public void setSeperatorToken(String seperatorToken){
+	public void setSeperatorToken(String seperatorToken) {
 		this.seperatorToken = seperatorToken;
 	}
+
 	/**
 	 * 
 	 * @return The current value of seperatorToken
 	 */
-	public String getSeperatorToken(){
+	public String getSeperatorToken() {
 		return seperatorToken;
 	}
+
 	/**
 	 * Set the ascending list style
+	 * 
 	 * @param isAscending
-	 * 				The new value of isAscending
+	 *            The new value of isAscending
 	 */
 	public void setAscending(boolean isAscending) {
 		this.isAscending = isAscending;
 	}
+
 	/**
 	 * 
 	 * @return The current value of isAscending
@@ -74,31 +113,17 @@ public class ListRule implements RenamerRule {
 	public boolean isAscending() {
 		return isAscending;
 	}
-	/**
-	 * Set the descending list style
-	 * 
-	 * @param isDescending
-	 * 				The new value of isDescending
-	 */
-	public void setDescending(boolean isDescending) {
-		this.isDescending = isDescending;
-	}
-	/**
-	 * 
-	 * @return The current value of isDescending
-	 */
-	public boolean isDescending() {
-		return isDescending;
-	}
+
 	/**
 	 * Set the add to end list style
 	 * 
-	 * @param addToEnd 
-	 * 				The new value of addToEnd
+	 * @param addToEnd
+	 *            The new value of addToEnd
 	 */
 	public void setAddToEnd(boolean isAddToEnd) {
 		this.isAddToEnd = isAddToEnd;
 	}
+
 	/**
 	 * 
 	 * @return The value of isAddToEnd
@@ -106,31 +131,17 @@ public class ListRule implements RenamerRule {
 	public boolean isAddToEnd() {
 		return isAddToEnd;
 	}
-	/**
-	 * Set the add to start list style
-	 * 
-	 * @param isAddToStart
-	 * 				The new value of isAddToStart
-	 */
-	public void setAddToStart(boolean isAddToStart) {
-		this.isAddToStart = isAddToStart;
-	}
-	/**
-	 * 
-	 * @return The value of isAddToStart
-	 */
-	public boolean isAddToStart() {
-		return isAddToStart;
-	}
+
 	/**
 	 * Set the value of the listStyleState, from ListStyle enumeration
 	 * 
 	 * @param listStyleState
-	 * 					The new value of listStyleState
+	 *            The new value of listStyleState
 	 */
 	public void setListStyleState(ListStyle listStyleState) {
 		this.listStyleState = listStyleState;
 	}
+
 	/**
 	 * 
 	 * @return The Value of listStyleState
@@ -138,15 +149,17 @@ public class ListRule implements RenamerRule {
 	public ListStyle getListStyleState() {
 		return listStyleState;
 	}
+
 	/**
 	 * Set the number of digits displayed in the list
 	 * 
 	 * @param digitsDisplayed
-	 * 					The new value of digitsDisplayed
+	 *            The new value of digitsDisplayed
 	 */
 	public void setDigitsDisplayed(int digitsDisplayed) {
 		this.digitsDisplayed = digitsDisplayed;
 	}
+
 	/**
 	 * 
 	 * @return The current value of digitsDisplayed
@@ -157,21 +170,13 @@ public class ListRule implements RenamerRule {
 
 	@Override
 	public void setup() {
-		// TODO Throw an error if the start from value does not match the listStyle
-		
+		counter = startFrom;
 	}
 
 	@Override
 	public void tearDown() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
 
 }
