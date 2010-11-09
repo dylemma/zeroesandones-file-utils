0ab3d3cdfee28ff74773109dcc9060bcbd0eec81
jsavickas103@gmail.com <jsavickas103@gmail.com@fd496d0a-aefe-dfaf-78f1-ce77a1763a5f>
2010-11-09 18:47:56 +0000

    Added classes for undo/redo: RenamerHistory is the class for undoing and redoing the entire history of RenamingEvents RenamerEvent is a collection of before and after files for a single rename I'm pretty sure i don't have the best practice for the defining of the next class: ChangedFile is a helper class for renamer event, containing before and after full paths to a single Changed file
    So to recap:
    Make a RenamerHistory object to keep track of the whole history.
    Add a RenamingEvent to the RenamerHistory after each applyChanges().
    While in applyChanges, make a new ChangedFile after each file rename.

---
+++ file-utils/src/edu/zao/fire/RenamedFile.java
@@ -0,0 +1,19 @@
+package edu.zao.fire;
+
+public class RenamedFile {
+	public String beforeFullPath;
+	public String afterFullPath;
+
+	/**
+	 * A helper class for the Renamer history, stores before and after paths
+	 * 
+	 * @param beforeFullPath
+	 *            Is the full canonical path to the file before its change
+	 * @param afterName
+	 *            Is the full canonical path to the file after its change
+	 */
+	RenamedFile(String beforeFullPath, String afterFullPath) {
+		this.beforeFullPath = beforeFullPath;
+		this.afterFullPath = afterFullPath;
+	}
+};
