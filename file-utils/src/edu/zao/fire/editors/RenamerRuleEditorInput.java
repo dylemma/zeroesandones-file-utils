464716ee4215b558b92fa8c383516ca57b08f484
pianomanDylan <pianomanDylan@fd496d0a-aefe-dfaf-78f1-ce77a1763a5f>
2010-11-10 04:26:23 +0000

    More javadoc comments.

--- file-utils/src/edu/zao/fire/editors/RenamerRuleEditorInput.java
+++ file-utils/src/edu/zao/fire/editors/RenamerRuleEditorInput.java
@@ -11,12 +11,31 @@ import org.eclipse.ui.IPersistableElement;
 
 import edu.zao.fire.RenamerRule;
 
+/**
+ * Common ground for all inputs to RenamerRuleEditors. Inputs must have a save
+ * file (which may be null if no rule has been saved yet). Basic saving
+ * functionality for renamer rules is implemented here.
+ * 
+ * @author Dylan
+ * 
+ */
 public abstract class RenamerRuleEditorInput implements IEditorInput {
 
 	protected File saveFile = null;
 
+	/**
+	 * @return The text to be displayed in the rule editor's title tab before
+	 *         the rule has been saved to a file.
+	 */
 	public abstract String getDefaultName();
 
+	/**
+	 * @return The text to be displayed in the rule editor's title tab. If the
+	 *         rule has been saved to a file, the title will be that file's
+	 *         name. Otherwise, whatever value returned by
+	 *         {@link #getDefaultName()} will be chosen.
+	 */
+	@Override
 	public String getName() {
 		if (saveFile == null) {
 			return getDefaultName();
@@ -24,6 +43,16 @@ public abstract class RenamerRuleEditorInput implements IEditorInput {
 		return saveFile.getName();
 	};
 
+	/**
+	 * Save the given rule to the given file, using Object serialization. Due to
+	 * java-related overhead, the save files are generally in the neighborhood
+	 * of a kilobyte, but the process is fast and painless.
+	 * 
+	 * @param rule
+	 * @param file
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
 	public void saveRuleToFile(RenamerRule rule, File file) throws FileNotFoundException, IOException {
 		saveFile = file;
 		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
--- file-utils/src/edu/zao/fire/util/FileGatherer.java
+++ file-utils/src/edu/zao/fire/util/FileGatherer.java
@@ -6,29 +6,79 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Helper class used for gathering a list of files that match an optional
+ * FileFilter, within a given starting directory.
+ * 
+ * @author Dylan
+ * 
+ */
 public class FileGatherer implements Iterable<File> {
 	private final File m_root;
 	private final FileFilter m_filter;
 	private final List<File> m_files = new LinkedList<File>();
 
+	/**
+	 * Constructor. Initializes the FileGatherer at the given rootPath, with no
+	 * file filter.
+	 * 
+	 * @param rootPath
+	 *            The path to the directory where the file gatherer will get its
+	 *            files.
+	 */
 	public FileGatherer(String rootPath) {
 		this(new File(rootPath), null);
 	}
 
+	/**
+	 * Constructor. Initializes the FileGatherer at the given rootPath and with
+	 * the given filter.
+	 * 
+	 * @param rootPath
+	 *            The path to the directory where the file gatherer will get its
+	 *            files.
+	 * @param filter
+	 *            The filter that chooses whether to accept (i.e. "gather") a
+	 *            given file.
+	 */
 	public FileGatherer(String rootPath, FileFilter filter) {
 		this(new File(rootPath), filter);
 	}
 
+	/**
+	 * Constructor. Initializes the FileGatherer at the given root directory,
+	 * with no file filter.
+	 * 
+	 * @param root
+	 *            The directory from which the gatherer "gathers" its files.
+	 */
 	public FileGatherer(File root) {
 		this(root, null);
 	}
 
+	/**
+	 * Constructor. Initializes the FileGatherer at the given rootPath and with
+	 * the given filter.
+	 * 
+	 * @param root
+	 *            The directory from which the gatherer "gathers" its files.
+	 * @param filter
+	 *            The filter that chooses whether to accept (i.e. "gather") a
+	 *            given file.
+	 */
 	public FileGatherer(File root, FileFilter filter) {
 		m_root = root;
 		m_filter = filter;
 		initializeFileList(root);
 	}
 
+	/**
+	 * Gather up all of the files under the root directory that match the
+	 * filter. If the filter is null, all of the files will be gathered.
+	 * 
+	 * @param root
+	 *            The directory from which the gatherer "gathers" its files.
+	 */
 	private void initializeFileList(File root) {
 		File[] files = root.listFiles();
 		if (files == null) {
@@ -45,6 +95,10 @@ public class FileGatherer implements Iterable<File> {
 		}
 	}
 
+	/**
+	 * @return An iterator over the files that were "gathered" by this
+	 *         FileGatherer.
+	 */
 	@Override
 	public Iterator<File> iterator() {
 		return m_files.iterator();
