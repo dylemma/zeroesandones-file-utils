43df487a23cf3b08e6a08207d74c2a1b33cb2fb2
oneilc4@rpi.edu <oneilc4@rpi.edu@fd496d0a-aefe-dfaf-78f1-ce77a1763a5f>
2010-11-17 23:35:46 +0000

    Implemented the MetadataRule class

--- trunk/file-utils/src/edu/zao/fire/MetadataRule.java
+++ trunk/file-utils/src/edu/zao/fire/MetadataRule.java
@@ -3,12 +3,65 @@ package edu.zao.fire;
 import java.io.File;
 import java.io.IOException;
 
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.TagException;
+
+import edu.zao.fire.editors.metadata.MetadataTag;
+import edu.zao.fire.editors.metadata.MetadataTagList;
+
 public class MetadataRule implements RenamerRule {
+
+	private MetadataTagList tagList;
 
 	@Override
 	public String getNewName(File file) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		String fileName = file.getName();
+		String newName = "";
+		String extension = "";
+
+		// getlastindexof? make sure to keep the extension name, so look for the
+		// '.'
+		int dotIndex = fileName.lastIndexOf(".");
+		if (dotIndex >= 0) {
+			extension = fileName.substring(dotIndex);
+		}
+
+		// HERE call the methods to generate the newName
+		try {
+			Tag songTag = getTagFromFile(file);
+			for (MetadataTag tag : tagList.getTags()) {
+				FieldKey key = tag.getFieldKey();
+				// pull the values out, tack them on to the newName
+
+				String value = songTag.getFirst(key);
+				newName += value;
+			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return file.getName();
+		}
+
+		// pull off the extension, then tack it back on in the end
+		newName = newName + extension;
+		return newName;
+	}
+
+	public Tag getTagFromFile(File inputSong) throws CannotReadException, IOException, TagException, ReadOnlyFileException,
+			InvalidAudioFrameException {
+
+		AudioFile song = AudioFileIO.read(inputSong);
+		Tag songTag = song.getTag();
+
+		return songTag;
+
 	}
 
 	@Override
@@ -21,6 +74,15 @@ public class MetadataRule implements RenamerRule {
 	public void tearDown() {
 		// TODO Auto-generated method stub
 
+	}
+
+	public MetadataTagList getTagList() {
+
+		return tagList;
+	}
+
+	public void setTagList(MetadataTagList tagList) {
+		this.tagList = tagList;
 	}
 
 }
