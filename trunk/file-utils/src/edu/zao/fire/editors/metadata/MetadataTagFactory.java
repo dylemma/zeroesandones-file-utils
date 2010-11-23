a3ec62025a1f7667a599b9d141308e892ebd5a01
pianomanDylan <pianomanDylan@fd496d0a-aefe-dfaf-78f1-ce77a1763a5f>
2010-11-23 22:11:09 +0000

    Fixed Issue 8. Decoupled the MetadataRule with Tags, decoupled MetadataTag with FieldKeys. Made a singleton class (MetadataTagCache) to keep track of files that JAudioTagger has parsed (so that work isn't done over and over- it takes too long for that to fly), instead of keeping the cache within the MetadataRule. This will actually help, so that two separate metadata rules are sharing information! Hooray. Also we can use the Singleton class as one of our "best practices". Basically, anything that was a member variable for a MetadataRule would be serialized, and eventually it got down to JAudioTagger stuff, which wasn't serializable, so it died. This gets rid of those dependencies.

--- trunk/file-utils/src/edu/zao/fire/editors/metadata/MetadataTagFactory.java
+++ trunk/file-utils/src/edu/zao/fire/editors/metadata/MetadataTagFactory.java
@@ -3,6 +3,8 @@ package edu.zao.fire.editors.metadata;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.jaudiotagger.tag.FieldKey;
+
 public class MetadataTagFactory {
 
 	private final static Map<String, MetadataTag> singletonTags = new TreeMap<String, MetadataTag>();
@@ -21,8 +23,24 @@ public class MetadataTagFactory {
 			return singletonTags.get(tagName);
 		}
 		if (tagName == MetadataTagNames.PLAINTEXT) {
-			return MetadataTag.makePlainTextTag("--");
+			return MetadataTag.makePlainTextTag("-");
 		}
-		return new MetadataTag(tagName, null, "Unknown " + tagName);
+		return new MetadataTag(tagName, "Unknown " + tagName);
 	}
+
+	private final static Map<String, FieldKey> fieldKeyMap = new TreeMap<String, FieldKey>();
+	static {
+		fieldKeyMap.put(MetadataTagNames.ALBUM, FieldKey.ALBUM);
+		fieldKeyMap.put(MetadataTagNames.ARTIST, FieldKey.ARTIST);
+		fieldKeyMap.put(MetadataTagNames.COMMENT, FieldKey.COMMENT);
+		fieldKeyMap.put(MetadataTagNames.COMPOSER, FieldKey.COMPOSER);
+		fieldKeyMap.put(MetadataTagNames.TITLE, FieldKey.TITLE);
+		fieldKeyMap.put(MetadataTagNames.TRACK, FieldKey.TRACK);
+		fieldKeyMap.put(MetadataTagNames.YEAR, FieldKey.YEAR);
+	}
+
+	public static FieldKey getFieldKey(String keyName) {
+		return fieldKeyMap.get(keyName);
+	}
+
 }
